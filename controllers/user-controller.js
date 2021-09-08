10c382873ca6da220f6ed19467f27da2f271af8b
William Takes <william.e.takes@vanderbilt.edu>
2021-09-08 15:58:19 -0500

    user controller file

---
+++ controllers/user-controller.js
@@ -0,0 +1,38 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+  // get all pizzas
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+  // get one pizza by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+   // createPizza
+   createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
