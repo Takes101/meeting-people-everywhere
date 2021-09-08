d62ca3e890306c14ef2a3238a5b1bb84678a91ce
William Takes <william.e.takes@vanderbilt.edu>
2021-09-08 15:41:07 -0500

    reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280
+    },
+    writtenBy: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+module.exports = ReactionSchema;
